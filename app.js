0dc68b895457751b281305ae9ca9fd0d0ec9be80
Have Samuel <samiehave9@gmail.com>
2023-05-04 03:51:39 +0300

    jQuery Text, HTML, Attributes, and Values

--- app.js
+++ app.js
@@ -33,7 +33,24 @@ $('h1'); // returns the h1
 $('h1').text(); // returns the text inside the h1
 $('h1').text('New Text!!!'); // changes the text inside the h1
 $('ul').text(); // returns all the text inside the ul
-
-// Using HTML()
 $('li'); // returns all the li's
 $('li').text('Have@#'); // changes the text inside all the li's
+
+// Using HTML()
+$('li').html(); // returns all the html inside the li's
+$('li').html('<b>BolDD!</b>'); // changes the html inside all the li's
+
+// Using ATTR()
+$('a').attr('href'); // returns the href of the first a tag
+$('a').attr('href', 'http://www.chichennuggets.com'); // changes the href of all a tags
+
+// Changing the img src to have one image
+const attImgs = { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/La_R%C3%A9union_department_relief_location_map.jpg/512px-La_R%C3%A9union_department_relief_location_map.jpg ', alt: 'Lava Flow' };
+
+$('img').attr(attImgs); // changes the src of all images
+
+// Using VAL()
+$('input'); // returns all the inputs
+$('input').val(); // returns the value of the first input
+$('input').eq(0).val('BAMBOOZLED'); // changes the value of the first input
+$('input').val(''); // changes the value of all inputs to empty string
