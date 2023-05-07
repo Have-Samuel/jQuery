22ada808ed95e18ceaf6f678c7d8866d8003fb65
Have Samuel <samiehave9@gmail.com>
2023-05-07 23:36:08 +0300

    Changing the Title

--- jquery.js
+++ jquery.js
@@ -1,2 +1,14 @@
 // Remove the Last Pararaph
-$('p').last().remove();
+$('p').last().remove(); // Last paragraph is removed
+
+// Resizing the title
+const heading = {
+  fontSize: '3em',
+  color: 'magenta',
+  backgroundColor: 'teal',
+  padding: '2rem',
+  borderRadius: '0.5rem',
+  'text-shadow': '0.1rem 0.1rem 0.2rem black',
+};
+
+$('h1').css(heading);
