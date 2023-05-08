eaaf2f54db46924ced50e0d0201b53227c1804ad
Have Samuel <samiehave9@gmail.com>
2023-05-08 03:41:57 +0300

    Fixing the background Change

--- jquery.js
+++ jquery.js
@@ -26,13 +26,10 @@ $(() => {
   // When you change the numbers in the three inputs on the bottom,
   // the background color of the body should change to match whatever
   // the three values in the inputs are.
-  $('#form-js').on('focus', 'input', function (e) {
-    let red = $('.form-control').eq(0).val();
-    let green = $('.form-control').eq(1).val();
-    let blue = $('.form-control').eq(2).val();
-
-
+  $('#form-js').on('focus', 'input', function () {
+    const red = $('.form-control').eq(0).val();
+    const green = $('.form-control').eq(1).val();
+    const blue = $('.form-control').eq(2).val();
     $('body').css('background-color', `rgb(${red}, ${green}, ${blue})`);
-  })
+  });
 });
-
