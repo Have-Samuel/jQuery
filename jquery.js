25b5e19c0000c9ce6ea00df5885b13934ad570df
Have Samuel <samiehave9@gmail.com>
2023-05-08 03:17:16 +0300

    Setting the inut fields to the interaction

--- jquery.js
+++ jquery.js
@@ -1,4 +1,4 @@
-$(function () {
+$(() => {
   console.log('Let\'s get ready to party with jQuery!'); // Logs Let's get ready to party with jQuery!
 
   $('img').addClass('image-center '); // Adds the class image-center to the img
@@ -7,25 +7,32 @@ $(function () {
 
   $('#title').css('font-size', Math.random() * 100);
 
-  $('ol').append('<li>It\s Coll Working With JQuery!</li>');
+  $('ol').append('<li>It\'s Coll Working With JQuery!</li>');
 
   $('ol').css('background-color', '#ccc');
 
-  $('aside').html('<p>paragraph in it apologizing for the list\'s existence</p>').css('font-size', '3rem', 'color', 'red');
-
-  $('.form-control').on('focus', function () {
-    const red = $('.form-control').eq(0).val();
-    const blue = $('.form-control').eq(1).val();
-    const green = $('.form-control').eq(2).val();
-    $('body').css('background-color', `rgb(${red}, ${blue}, ${green})`);
-  });
+  $('aside').empty().append('<p>paragraph in it apologizing for the list\'s existence</p>')
+    .css('color', 'red');
 
   $('img').on('click', function () {
     $(this).animate({
       opacity: 0.25,
-      height: 'toggle'
+      height: 'toggle',
     }, 2000, function () {
       $(this).remove();
     });
-    });
+  });
+
+  // When you change the numbers in the three inputs on the bottom,
+  // the background color of the body should change to match whatever
+  // the three values in the inputs are.
+  $('#form-js').on('focus', 'input', function (e) {
+    let red = $('.form-control').eq(0).val();
+    let green = $('.form-control').eq(1).val();
+    let blue = $('.form-control').eq(2).val();
+
+
+    $('body').css('background-color', `rgb(${red}, ${green}, ${blue})`);
+  })
 });
+
