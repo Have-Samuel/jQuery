e58b4d5a64dde3508de29cfadd07c1b44224c5de
Have Samuel <samiehave9@gmail.com>
2023-05-17 01:48:24 +0300

    Add 'image-center' to the Image

---
+++ jquery.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+$(() => {
+  console.log('Let\'s get ready to party with jQuery!');
+
+  // Add 'image-center' to the Image
+  $('article img').addClass('image-center');
+});
